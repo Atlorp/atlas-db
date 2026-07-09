90d4e9d1421054cf1f07cac3879254d1d9b2333d
venkeyz <vennnkeyw@gmail.com>
2026-07-09 14:33:29 +0200

    add nso to the generate file blacklist and change gmod to dropbox

--- source/generate.py
+++ source/generate.py
@@ -31,7 +31,7 @@ from unistore import StoreEntry, UniStore, ICONS_PER_SHEET
 from utils import (format_to_web_name, format_traceback, get_matching_app,
                    to_friendly_bytes, was_recently_updated, create_installation_instructions)
 
-DOWNLOAD_BLACKLIST = r"(\.3ds$|\.apk|\.appimage|\.flatpak|\.dmg|\.dol|\.exe|\.ipa|\.love|\.nro|\.opk|\.pkg|\.smdh|\.vpk|\.xz|armhf|elf|linux|macos|osx|PS3|PSP|switch|ubuntu|vita|wii|win|x86_64|xbox)"
+DOWNLOAD_BLACKLIST = r"(\.3ds$|\.apk|\.appimage|\.flatpak|\.dmg|\.dol|\.exe|\.ipa|\.love|\.nro|\.opk|\.pkg|\.smdh|\.vpk|\.xz|\.nso|armhf|elf|linux|macos|osx|PS3|PSP|switch|ubuntu|vita|wii|win|x86_64|xbox)"
 DOCS_DIR: Optional[pathlib.Path] = None
 PRIORITY_MODE = True
 SCRIPT_DIR = pathlib.Path(__file__).parent.resolve()
