a023e041e6de98c18f793e3e10d60810d65a8273
venkeyz <vennnkeyw@gmail.com>
2026-07-10 03:57:27 +0200

    we dont need that much custom languages

--- crowdin-pull.sh
+++ crowdin-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add new languages here, space separated and using the ID for `crowdin pull`
-LANGUAGES="bruh de es-ES fr he hu it ja ko no pl pt-BR ro ru ry tr uk zh-CN zh-TW"
+LANGUAGES="bruh de es-ES fr he hu it ja ko no pl pt-BR ro ru tr uk zh-CN zh-TW"
 
 ARG=''
 for LANGUAGE in $LANGUAGES; do
