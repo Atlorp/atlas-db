78f1be1dbf65fa4882152c43e669d1087931510d
pixelyloaf <182298061+pixelyloaf@users.noreply.github.com>
2026-07-16 05:15:38 +0100

    why do we have this???

--- crowdin-pull.sh
+++ crowdin-pull.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add new languages here, space separated and using the ID for `crowdin pull`
-LANGUAGES="bruh de es-ES fr he hu in-context it ja ko no pl pt-BR ro ru ry tr uk zh-CN zh-TW lol"
+LANGUAGES="bruh de es-ES fr he hu it ja ko no pl pt-BR ro ru ry tr uk zh-CN zh-TW lol"
 
 ARG=''
 for LANGUAGE in $LANGUAGES; do
